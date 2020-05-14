535f835153ed1bfef22885f454b64f8a4f1cb2ea
samjat2012cl <samjat2012cl@gmail.com>
2020-05-14 13:35:59 -0700

    cleaning unnecessory files

--- notebooks/dnn_infer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for manifest_path in iconect/csvs_orig/*
-do
-    manifest="$(basename "$manifest_path")"
-    echo ${manifest}
-    python ./dnn_infer.py ${manifest} || exit 0;
-    break
-done
